135e405f6b3e71de63ef51776f05599b41ba8f5c
Eddison Chen <eddisonchen1@gmail.com>
2023-01-04 01:17:22 -0500

    created react and node app

---
+++ front-end/calorie-front-end/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.scss';
+
+function App() {
+  return (
+    <body>
+        <h1>Calorie Calculator</h1>
+    </body>
+  );
+}
+
+export default App;
+
+// core features
+// fetch request to pull calorie information from searched foods
+// pull information from restaurants
+// calculate tdee and your calories that you want to eat
+// track macro/micronutrients
