533f508ed6f3cef063f29df74342817f81f1173d
Eddison Chen <eddisonchen1@gmail.com>
2023-01-17 18:20:20 -0500

    added some variables

--- front-end/calorie-front-end/src/containers/BodyStats/BodyStats.jsx
+++ front-end/calorie-front-end/src/containers/BodyStats/BodyStats.jsx
@@ -4,8 +4,10 @@ import { useState, useEffect } from 'react'
 const BodyStats = (props) => {
 
     const [bf, setBf] = useState()
+    const [calculatedCalories, setCalculatedCalories] = useState()
 
     const bodyStats = {
+        "unitType" : null,
         "sex": null,
         "age": null,
         "weight": null,
@@ -20,22 +22,38 @@ const BodyStats = (props) => {
     }
 
     const calculateCalories = () => {
+        let sexValue = null;
+        bodyStats["sex"] == "male" ? sexValue = 5 : sexValue = -151;
+        if (bodyStats["bodyFatPercentage"] !== null) {
+            setCalculatedCalories()
+        } else {
+            setCalculatedCalories()
+        }
 
     }
 
 
-    
-
     return (
         <div>
             <h2>Please input your stats</h2>
+                <div>
+                    <input onClick={setBodyStats} type="radio" name="unitType" value="imperial"></input>Imperial
+                    <input onClick={setBodyStats} type="radio" name="unitType" value="metric"></input>Metric
+                </div>
             <div>
                 <input onClick={setBodyStats} type="radio" name="sex" value="male"></input>Male
                 <input onClick={setBodyStats} type="radio" name="sex" value="female"></input>Female
-                <input onChange={setBodyStats} type="textbox" name="age" pattern="[[0-9]|[1-9][0-9]|[1-9][0-9][0-9]]"></input>
-                <input onChange={setBodyStats} type="textbox" name="weight"></input>
-                <input onChange={setBodyStats} type="textbox" name="height"></input>
-                <input onChange={setBodyStats} type="textbox" name="Body Fat Percentage"></input>
+                <input onChange={setBodyStats} type="number" name="age" placeholder="Age"></input>
+                <input onChange={setBodyStats} type="number" name="weight" placeholder="Weight"></input>
+                <input onChange={setBodyStats} type="number" name="height" placeholder="Height"></input>
+                <input onChange={setBodyStats} type="number" name="Body Fat Percentage" placeholder="Body Fat Percentage (Optional)"></input>
+                <ul>Activity Level</ul>
+                    <li>Sedentary</li>
+                    <li>Lightly Active</li>
+                    <li>Moderately Active</li>
+                    <li>Active</li>
+                    <li>Highly Active</li>
+
                 <button>Submit</button>
             </div>
 
