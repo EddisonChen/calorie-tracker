f2f14b2cbb9eade3e69b2171b794c0c927703d4d
Eddison Chen <eddisonchen1@gmail.com>
2023-01-19 01:22:49 -0500

    added formulas

--- front-end/calorie-front-end/src/containers/BodyStats/BodyStats.jsx
+++ front-end/calorie-front-end/src/containers/BodyStats/BodyStats.jsx
@@ -5,6 +5,7 @@ const BodyStats = (props) => {
 
     const [bf, setBf] = useState()
     const [calculatedCalories, setCalculatedCalories] = useState()
+    const [output, setOutput] = useState('')
 
     const bodyStats = {
         "unitType" : null,
@@ -17,20 +18,39 @@ const BodyStats = (props) => {
     }
 
     const setBodyStats = (event) => {
-        bodyStats[event.target.name] = event.target.value
+        if (event.target.name == "unitType" || event.target.name == "sex" || event.target.name == "activityLevel") {
+            bodyStats[event.target.name] = event.target.value
+        } else {
+            bodyStats[event.target.name] = Number(event.target.value)
+        }
         console.log(bodyStats)
     }
 
     const calculateCalories = () => {
-        let sexValue = null;
-        bodyStats["sex"] == "male" ? sexValue = 5 : sexValue = -151;
-        if (bodyStats["bodyFatPercentage"] !== null) {
-            setCalculatedCalories()
-        } else {
-            setCalculatedCalories()
+        if (bodyStats["unitType"] == "imperial") {
+            bodyStats["weight"] = bodyStats["weight"]/2.205
+            bodyStats["height"] = bodyStats["height"] * 2.54
         }
-
+        if (bodyStats["unitType"] !== null || bodyStats["sex"] !== null || bodyStats["age"] !== null || bodyStats["weight"] !== null || bodyStats["height"] !== null || bodyStats["activityLevel"] !== null) {
+            if (bodyStats["bodyFatPercentage"] == null) {
+                let sexValue = null;
+                bodyStats["sex"] == "male" ? sexValue = 5 : sexValue = -151;
+                setCalculatedCalories(Math.round((10*bodyStats["weight"]) + (6.25*bodyStats["height"]) - 5*bodyStats["age"] - sexValue))
+            } else {
+                const leanBodyMass = (1 - bodyStats["bodyFatPercentage"]/10) * bodyStats["weight"]
+                setCalculatedCalories(Math.round(370 + (21.6 * leanBodyMass)))
+            }
+        
+        } else {
+            setOutput("Hey, fill out all the forms")
+        }
     }
+
+    const determineDisplayedMessage = () => {
+        setOutput(calculatedCalories)
+    }
+
+    useEffect(determineDisplayedMessage, [calculatedCalories])
 
 
     return (
@@ -47,14 +67,16 @@ const BodyStats = (props) => {
                 <input onChange={setBodyStats} type="number" name="weight" placeholder="Weight"></input>
                 <input onChange={setBodyStats} type="number" name="height" placeholder="Height"></input>
                 <input onChange={setBodyStats} type="number" name="Body Fat Percentage" placeholder="Body Fat Percentage (Optional)"></input>
-                <ul>Activity Level</ul>
-                    <li>Sedentary</li>
-                    <li>Lightly Active</li>
-                    <li>Moderately Active</li>
-                    <li>Active</li>
-                    <li>Highly Active</li>
-
-                <button>Submit</button>
+                <h3>Activity Level</h3>
+                    <input onClick={setBodyStats} type="radio" name="activityLevel" value="sedentary"></input>Sedentary
+                    <input onClick={setBodyStats} type="radio" name="activityLevel" value="lightly active"></input>Lightly Active
+                    <input onClick={setBodyStats} type="radio" name="activityLevel" value="moderately active"></input>Moderately Active
+                    <input onClick={setBodyStats} type="radio" name="activityLevel" value="highly active"></input>Highly Active
+                    <input onClick={setBodyStats} type="radio" name="activityLevel" value="athlete"></input>Athlete
+                <button onClick={calculateCalories}>Submit</button>
+            </div>
+            <div>
+                {output ? output : ''}
             </div>
 
         </div>
